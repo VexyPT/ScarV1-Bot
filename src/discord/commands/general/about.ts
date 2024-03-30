3c4893b3c9085f772a1c698bd5385a40412b70e5
Daniel <danifilipe221205@gmail.com>
2024-03-30 00:20:11 +0000

    Create about.ts

---
+++ src/discord/commands/general/about.ts
@@ -0,0 +1,64 @@
+import { Command } from "#base";
+import { settings } from "#settings";
+import { createRow, hexToRgb } from "@magicyan/discord";
+import { ApplicationCommandType, ButtonBuilder, ButtonStyle, EmbedBuilder } from "discord.js";
+
+new Command({
+    name: "about",
+    description: "🏷️ Learn more about Scar",
+    dmPermission: false,
+    type: ApplicationCommandType.ChatInput,
+    async run(interaction){
+
+        const { client, member } = interaction;
+
+        const embed = new EmbedBuilder({
+            title: `🤖 About ${client.user.username}`,
+            description:
+            `Experience the pinnacle of server security, comfort, and entertainment • **Moderation, Backups, Giveaways, Ranks, Economy, Logs, Reaction Roles, Tickets, and more!** 🎉
+            
+            ${settings.links.invite ? `**If you want to invite Scar to your server, you can do so by clicking [here](${settings.links.invite})**` : ""}
+            `,
+            footer: {
+                text: `Requested by ${member.user.globalName || member.user.username}`,
+                iconURL: interaction.member.user.displayAvatarURL({
+                    size: 256,
+                }),
+            },
+            timestamp: new Date().toISOString(),
+            color: hexToRgb(settings.colors.azoxo),
+        });
+
+        if(settings.links.invite) {
+            const action = createRow(
+                new ButtonBuilder({
+                    label: "Dashboard",
+                    emoji: settings.emojis.static.partners.scar,
+                    style: ButtonStyle.Link,
+                    url: "https://google.com",
+                    disabled: true
+                }),
+                new ButtonBuilder({
+                    label: "Support Server",
+                    emoji: settings.emojis.static.support,
+                    style: ButtonStyle.Link,
+                    url: "https://discord.gg/y4c6JsTaMn",
+                    disabled: false
+                }),
+            );
+
+            interaction.reply({
+                embeds: [embed],
+                components: [action],
+                ephemeral: false
+            });
+            return;
+        }
+        else {
+            interaction.reply({
+                embeds: [embed],
+                ephemeral: false
+            });
+        }
+    }
+});
