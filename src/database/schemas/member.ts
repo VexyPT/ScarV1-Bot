d0f7f8af0e6c7e838b580b79c4ef8269559c74aa
Daniel <danifilipe221205@gmail.com>
2024-04-10 10:21:45 +0100

    Update member.ts

--- src/database/schemas/member.ts
+++ src/database/schemas/member.ts
@@ -1,18 +1,29 @@
 import { Schema } from "mongoose";
 import { t } from "../utils.js";
 
+export const transactionSchema = new Schema({
+    to: t.string,
+    from: t.string,
+    quantity: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+    received: { type: Boolean, default: true },
+    type: { type: String, default: "??" }
+});
+
 export const memberSchema = new Schema(
     {
         id: t.string,
-        guildId: t.string,
-        wallet: {
-            coins: { type: Number, default: 0 },
-        }
+        coins: { type: Number, default: 0 },
+        transactions: [transactionSchema],
+        isBanned: { type: Boolean, default: false },
+        banReason: { type: String, default: "Não definido" },
+        banDate: { type: Date, default: Date.now() },
+
     },
     {
         statics: {
-            async get(member: { id: string, guild: { id: string } }) {
-                const query = { id: member.id, guildId: member.guild.id };
+            async get(member: { id: string }) {
+                const query = { id: member.id };
                 return await this.findOne(query) ?? this.create(query);
             }
         }
